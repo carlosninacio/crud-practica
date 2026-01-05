f6d230ca4a39e417980cf2cfbc0d8011b37de2d2
carlosninacio <cigramirez@poligran.edu.co>
2026-01-05 14:18:11 -0500

    Caso 3 terminado

--- crud/src/main/java/org/example/crud/Main.java
+++ crud/src/main/java/org/example/crud/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println("=====================================");
 
         HashMap<Integer, Producto> productos = new HashMap<>();
+        HashMap<Integer, Producto> carrito = new HashMap<>();
         Scanner sc = new Scanner(System.in);
         boolean cerrar = false;
         int opcion = 0;
@@ -24,10 +25,9 @@ public class Main {
             System.out.println("\t1. Ver Productos");
             System.out.println("\t2. Agregar Productos");
             System.out.println("\t3. Eliminar Productos");
-            System.out.println("\t4. Ver Productos");
-            System.out.println("\t5. Agregar Productos al Carrito");
-            System.out.println("\t6. Facturar");
-            System.out.println("\t7. Salir");
+            System.out.println("\t4. Agregar Productos al Carrito");
+            System.out.println("\t5. Facturar");
+            System.out.println("\t6. Salir");
             opcion = Integer.parseInt(sc.nextLine());
 
             switch (opcion) {
@@ -51,6 +51,23 @@ public class Main {
                         System.out.println("\nProducto agregado correctamente.");
                     }
                     break;
+                case 3:
+                    System.out.print("Escribe el Id del producto que deseas eliminar: ");
+                    int idEliminar = Integer.parseInt(sc.nextLine());
+
+                    if (!productos.containsKey(idEliminar)) {
+                        System.out.println("ERROR: ID NO existente!");
+                    } else {
+                        productos.remove(idEliminar);
+                        System.out.println("Producto eliminado correctamente.");
+                    }
+                    break;
+                case 4:
+                    boolean compra = true;
+                    do {
+                        System.out.print("Escribe el Id del producto que deseas agregar al carrito: ");
+                        int idComprar = Integer.parseInt(sc.nextLine())
+                    } while (compra);
                 case 7:
                     cerrar = true;
                     break;
